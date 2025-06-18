24b0af1e9a0f01122c2a52e114a38000eea6660f
hassanFN <hchanna2@illinois.edu>
2025-06-18 21:31:08 +0500

    Update app.py
    starter code written for app.py

--- backend/dispatcher_service/app.py
+++ backend/dispatcher_service/app.py
@@ -1,1 +1,15 @@
+from kafka import KafkaConsumer
 
+TOPIC_NAME = 'dispatch-tasks'
+
+consumer = KafkaConsumer(
+    TOPIC_NAME,
+    bootstrap_servers='kafka:9092',
+    auto_offset_reset='earliest',
+    group_id='dispatch-group'
+)
+
+print(f"🚀 Listening for messages on '{TOPIC_NAME}'...")
+
+for message in consumer:
+    print(f"📦 Received: {message.value.decode('utf-8')}")
